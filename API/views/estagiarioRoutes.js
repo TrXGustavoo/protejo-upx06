43ade63e78766cec36c755ba31561f32e87bcbed
Gustavo Passos <gustavofp111@gmail.com>
2024-11-11 23:56:33 -0300

    Refatorando estagiario: adicionando funções para listar e excluir estagiários, atualizando o modelo e integrando com o frontend

--- API/views/estagiarioRoutes.js
+++ API/views/estagiarioRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const estagiarioController = require('../controllers/estagiarioController');
 
 router.post('/registrar', estagiarioController.registrar);
+router.get('/listar', estagiarioController.listar_estagiario);
+router.delete('/:id', estagiarioController.excluirEstagiario);
 
 module.exports = router;
--- Front/src/app/services/api.service.ts
+++ Front/src/app/services/api.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export interface Estagiario {
+  id: number;
   nome_completo: string;
   data_nascimento: string;
   email: string;
@@ -15,11 +16,22 @@ export interface Estagiario {
   providedIn: 'root'
 })
 export class EstagiarioService {
-  private apiUrl = 'http://localhost:3000/estagiarios/registrar';
+  private registrarUrl = 'http://localhost:3000/estagiarios/registrar';
+  private listarUrl = 'http://localhost:3000/estagiarios/listar';
+  private deletarUrl = 'http://localhost:3000/estagiarios/:id';
 
   constructor(private http: HttpClient) { }
 
   registrar(estagiario: Estagiario): Observable<any> {
-    return this.http.post(`${this.apiUrl}`, estagiario);
+    return this.http.post(`${this.registrarUrl}`, estagiario);
   }
+  listar_estagiario(): Observable<Estagiario[]> {
+    return this.http.get<Estagiario[]>(this.listarUrl);
+  }
+  excluirEstagiario(id: number): Observable<any> {
+    const url = `${this.deletarUrl.replace(':id', id.toString())}`; 
+    console.log("URL de exclusão:", url);  // Adicione este console.log para verificar a URL
+    return this.http.delete(url);
+  }
+  
 }
